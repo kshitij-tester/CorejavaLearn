70377fe4f8161d66d8802a3addcef7296720b684
salgarkshitij21-source <salgar.kshitij21@gmail.com>
2026-08-31 05:01:00 -0700

    Demo check

---
+++ JavaProjectWD1/src/basics/Abcd.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class Abcd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("This is GitHub session");
+	}
+
+}

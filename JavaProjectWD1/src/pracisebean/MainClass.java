a927c2aff89cd2c26bd44f4c7dfcd43f6be5f9f3
salgarkshitij21-source <salgar.kshitij21@gmail.com>
2026-08-31 04:58:25 -0700

    This is student details

---
+++ JavaProjectWD1/src/pracisebean/MainClass.java
@@ -0,0 +1,19 @@
+package pracisebean;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+StudBean1 s=new StudBean1();
+
+s.setStudrollno(28);
+s.setStudname("ABC");
+s.setStudfees(37373);
+
+System.out.println("StudRollno:- "+s.getStudrollno());
+System.out.println("Stud Name:- "+s.getStudname());
+System.out.println("Stud Fees:- "+s.getStudfees());
+
+	}
+
+}
---
+++ JavaProjectWD1/src/pracisebean/StudBean1.java
@@ -0,0 +1,32 @@
+package pracisebean;
+
+public class StudBean1 {
+
+	
+	
+	private int studrollno;
+	private String studname;
+	private double studfees;
+	public int getStudrollno() {
+		return studrollno;
+	}
+	public void setStudrollno(int studrollno) {
+		this.studrollno = studrollno;
+	}
+	public String getStudname() {
+		return studname;
+	}
+	public void setStudname(String studname) {
+		this.studname = studname;
+	}
+	public double getStudfees() {
+		return studfees;
+	}
+	public void setStudfees(double studfees) {
+		this.studfees = studfees;
+	}
+	
+	
+	
+	
+}
